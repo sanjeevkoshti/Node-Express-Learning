22e68af7e2e26ffc8232376899b622aa36b5cd0e
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-03-03 10:32:39 +0530

    interpolation syntax

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice", { diceVal });
 });
 
 app.listen(port, () => {
